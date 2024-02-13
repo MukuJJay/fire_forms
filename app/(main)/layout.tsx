eaab9f45bc8ecd567243e1c20b26fdfb232d82cc
Senku Sharma <mukujjee@gmail.com>
2024-02-13 20:22:54 +0530

    prisma schema has been set up

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,11 +1,22 @@
 import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div className="flex flex-col ">
-      <nav>
+      <nav className="flex p-4 items-center justify-between">
         <div>FIRE_FORMS</div>
-        <div></div>
+        <ul className="flex justify-center items-center gap-4">
+          <li>
+            <ThemeToggle />
+          </li>
+          <li>
+            <UserButton
+              appearance={{ elements: { avatarBox: "w-[40px] h-[40px]" } }}
+            />
+          </li>
+        </ul>
       </nav>
       {children}
     </div>
