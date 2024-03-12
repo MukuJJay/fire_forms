49d6dd94140317e3267e48142f526fd92f4a9a59
Mukul Mukhopadhyay <mukujjee@gmail.com>
2024-03-13 00:55:37 +0530

    astyles fixed

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import useMount from "@/hooks/useMount";
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/Fire Forms.png";
 
 const Logo = () => {
   const isMounted = useMount();
@@ -9,7 +11,11 @@ const Logo = () => {
 
   const origin = window.location.origin;
 
-  return <Link href={origin}>FireForms</Link>;
+  return (
+    <Link href={origin}>
+      <Image src={logo} alt="Fire Forms" width={50} height={50}></Image>
+    </Link>
+  );
 };
 
 export default Logo;
